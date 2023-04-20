3e5f216acdf68b36def3cb13f8c8ac766c7c9ddf
Wesley B. Johnson <wesleyjohnson773@gmail.com>
2023-04-20 14:31:21 -0600

    Firebase hosting deploy

--- src/firebase.js
+++ src/firebase.js
@@ -7,10 +7,12 @@ import 'firebase/firestore'
 const firebaseConfig = {
     apiKey: "AIzaSyBwICGc9ohu5tTIWTH8D-RkNzMwwcOm9zg",
     authDomain: "messaging-app-mern-8e69b.firebaseapp.com",
+    databaseURL: "https://messaging-app-mern-8e69b-default-rtdb.firebaseio.com",
     projectId: "messaging-app-mern-8e69b",
     storageBucket: "messaging-app-mern-8e69b.appspot.com",
     messagingSenderId: "1046774636440",
-    appId: "1:1046774636440:web:b8b3adb6070f087a2495ed"
+    appId: "1:1046774636440:web:b8b3adb6070f087a2495ed",
+    measurementId: "G-H4K9Y8L6PQ"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
